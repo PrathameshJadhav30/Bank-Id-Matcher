b19429f39801f268b6a8cca6d3c3fc58a848f869
Prathamesh <prathameshaj2004@gmail.com>
2024-09-09 15:13:02 +0530

    Changes

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def match_data():
     except Exception as e:
         return render_template('index.html', message=f"An error occurred: {e}")
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
